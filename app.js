9a360f8bef6a4b9915aab8d3c9094407e142eee1
Me\Mikinj <mikinj-mistry@hotmail.com>
2017-05-27 06:21:47 -0700

    mosca server

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var config = require('./config');
 var db = require('./models/db');
+var mosca = require('./mqttBroker');
 var fileUpload = require('express-fileupload');
 var expressValidator = require('express-validator');
 
