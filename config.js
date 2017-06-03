4013d8d56d204bc17d9796c9572f9b021cf4175b
DESKTOP-1814J89\Siddharth <sarfalesiddharth@gmail.com>
2017-06-03 17:49:29 +0530

    Perform basic changes related to configuration

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     "node_port": 4000,
-    "database": "mongodb://127.0.0.1:27017/gleekr",
+    "database": "mongodb://35.154.229.129:27017/gleekr",
     "TWILIO_ACCOUNT_SID" : "AC85e236476ea6edc6af5dd63bd677fadd",
     "TWILIO_AUTH_TOKEN" : "aa83bae89551d91d913347f33c40b68f",
     "TWILIO_NUMBER" : "+16629857397",
@@ -8,7 +8,7 @@ module.exports = {
     "REFRESH_TOKEN_SECRET_KEY" : "ZYXWVUTSRQPONMLKJIHGFEDCBA",
     "OTP_EXPIRETION": "15",
     "HOST": "127.0.0.1",
-    "REMOTE_HOST": "10.10.10.10",
+    "REMOTE_HOST": "35.154.229.129",
     "OK_STATUS": 200,
     "BAD_REQUEST": 400,
     "UNAUTHORIZED": 401,
