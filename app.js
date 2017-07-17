9d508ffc9295c51f5fb6fcd4ad238ad6ce0e1cf1
sarfalesiddharth <sarfalesiddharth@gmail.com>
2017-07-17 11:21:32 -0700

    Group api changes

--- app.js
+++ app.js
@@ -22,24 +22,29 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(bodyParserJsonError());
 
 // Custom validation
 app.use(expressValidator({
-  customValidators: {
-    startBefore: function (startDate, endDate) {
-      startDate = moment(startDate);
-      endDate = moment(endDate);
-      return moment(startDate).isSameOrBefore(endDate);
-    },
-    startDateTimeBefore: function (startDateTime, endDateTime) {
-      startDate = moment(startDateTime);
-      endDate = moment(endDateTime);
-      return moment(startDateTime).isBefore(endDateTime);
+    customValidators: {
+        startBefore: function (startDate, endDate) {
+            startDate = moment(startDate);
+            endDate = moment(endDate);
+            return moment(startDate).isSameOrBefore(endDate);
+        },
+        startDateTimeBefore: function (startDateTime, endDateTime) {
+            startDate = moment(startDateTime);
+            endDate = moment(endDateTime);
+            return moment(startDateTime).isBefore(endDateTime);
+        }, isArray: function (value) {
+            return Array.isArray(value);
+        },
+        notEmpty: function (array) {
+            return array.length > 0;
+        }
     }
-  }
 }));
 
 app.use(cookieParser());
@@ -51,9 +56,9 @@ app.use(require('./controllers'));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -61,26 +66,26 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-      message: err.message
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.json({
+            message: err.message
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message
-  });
+    res.status(err.status || 500);
+    res.json({
+        message: err.message
+    });
 });
 
 
 app.listen((config.node_port || 3000), function () {
-  console.log('Listening on port ' + (config.node_port || 3000) + '...');
+    console.log('Listening on port ' + (config.node_port || 3000) + '...');
 })
 
 module.exports = app;
--- controllers/group.js
+++ controllers/group.js
@@ -3,9 +3,11 @@ var router = express.Router();
 var config = require('../config');
 
 var Group = require("../models/group");
+var User = require("../models/user");
 
 var fs = require('fs');
 var path = require('path');
+var _ = require('underscore');
 
 /**
  * @api {post} /group Create Group
@@ -140,9 +142,9 @@ router.put('/', function (req, res, next) {
 /**
  * @api {delete} /group?id=:id Delete Group
  * @apiName Delete Group
- * @apiGroup User - READY
+ * @apiGroup Group - READY
  * 
- * @apiParam {String} id Activity id
+ * @apiParam {String} id Group id
  * 
  * @apiHeader {String}  x-access-token Users unique access-key
  *
@@ -168,6 +170,136 @@ router.delete('/', function (req, res, next) {
                 };
                 res.status(config.OK_STATUS).json(result);
             }
+        });
+    } else {
+        res.status(config.BAD_REQUEST).json({
+            message: "Validation Error ",
+            error: errors
+        });
+    }
+});
+/**
+ * @api {post} /add_member
+ * @apiName Add Group
+ * @apiGroup Group
+ * 
+ * @apiParam {Array} members array of userid to add as member
+ * @apiParam {String} id Group id
+ * 
+ * @apiHeader {String}  x-access-token Users unique access-key
+ *
+ * @apiSuccess (Success 200) {String} message Success message.
+ * @apiError (Error 4xx) {String} message Validation or error message.
+ */
+router.post('/add_member', function (req, res, next) {
+    var schema = {
+        'members': {
+            notEmpty: true,
+            errorMessage: "Members are required"
+        },
+        'id': {
+            notEmpty: true,
+            errorMessage: "group id is required"
+        }
+    };
+    req.checkBody(schema);
+    var errors = req.validationErrors();
+    if (!errors) {
+        var arr = _.map(req.body.members, function(val) {
+            return {'user_id' : val};
+        });
+        Group.findOneAndUpdate({_id: req.body.id}, {
+            $pushAll: {
+                'members' : arr
+            }
+        }, function (err, data) {
+            if (err) {
+                return next(err);
+            }
+            res.status(config.OK_STATUS).json({'message' : 'Member successfully added.'});
+        });
+    } else {
+        res.status(config.BAD_REQUEST).json({
+            message: "Validation Error ",
+            error: errors
+        });
+    }
+});
+/**
+ * @api {post} /remove_member
+ * @apiName Delete Group member
+ * @apiGroup Group
+ * 
+ * @apiParam {Array} members array of userid to add as member
+ * @apiParam {String} id Group id
+ * 
+ * @apiHeader {String}  x-access-token Users unique access-key
+ *
+ * @apiSuccess (Success 200) {String} message Success message.
+ * @apiError (Error 4xx) {String} message Validation or error message.
+ */
+router.post('/remove_member', function (req, res, next) {
+    var schema = {
+        'members': {
+            notEmpty: true,
+            errorMessage: "Members are required"
+        },
+        'id': {
+            notEmpty: true,
+            errorMessage: "group id is required"
+        }
+    };
+    req.checkBody(schema);
+    var errors = req.validationErrors();
+    if (!errors) {
+        Group.findOneAndUpdate({_id: req.body.id}, {
+            $pull: {
+                'members' : {'user_id'  : { '$in' : req.body.members}}
+            }
+        }, function (err, data) {
+            if (err) {
+                return next(err);
+            }
+            res.status(config.OK_STATUS).json({'message' : 'Member successfully removed.'});
+        });
+    } else {
+        res.status(config.BAD_REQUEST).json({
+            message: "Validation Error ",
+            error: errors
+        });
+    }
+});
+/**
+ * @api {post} /exit_group
+ * @apiName Exit Group
+ * @apiGroup Group
+ * 
+ * @apiParam {String} id Group id
+ * 
+ * @apiHeader {String}  x-access-token Users unique access-key
+ *
+ * @apiSuccess (Success 200) {String} message Success message.
+ * @apiError (Error 4xx) {String} message Validation or error message.
+ */
+router.post('/exit_group', function (req, res, next) {
+    var schema = {
+        'id': {
+            notEmpty: true,
+            errorMessage: "group id is required"
+        }
+    };
+    req.checkBody(schema);
+    var errors = req.validationErrors();
+    if (!errors) {
+        Group.findOneAndUpdate({_id: req.body.id}, {
+            $pull: {
+                'members' : {'user_id'  : { '$eq' : req.userInfo.id}}
+            }
+        }, function (err, data) {
+            if (err) {
+                return next(err);
+            }
+            res.status(config.OK_STATUS).json({'message' : 'Exit from group successfully.'});
         });
     } else {
         res.status(config.BAD_REQUEST).json({
