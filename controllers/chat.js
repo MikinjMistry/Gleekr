7de72d06c00af7c782211f2d8ab201a9e3b7a6d0
sarfalesiddharth <sarfalesiddharth@gmail.com>
2017-07-29 04:24:05 -0700

    import chat - inprogress

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var config = require('../config');
+var rmdir = require('rmdir');
 
 var Activity = require("../models/activity");
 var User = require("../models/user");
@@ -16,7 +17,70 @@ var path = require('path');
 var async = require('async');
 var _ = require('underscore');
 
-
+/**
+ * @api {post} /import Import your chat file
+ * @apiName Imort Chat
+ * @apiGroup Import / Export
+ * @apiDescription You need to pass Form Data
+ * 
+ * @apiParam {file} file form-data: file object
+ *  
+ * @apiHeader {String}  x-access-token Users unique access-key
+ * 
+ * @apiSuccess (Success 200) {String} message Success message.
+ * @apiError (Error 4xx) {String} message Validation or error message.
+ */
+router.post('/import', function (req, res, next) {
+    var errors = req.validationErrors();
+    if (!errors) {
+        var json = req.body;
+        var userInfo = req.userInfo;
+        json.user_id = userInfo.id;
+        if (req.files) {
+            var file = req.files.file;
+            var dir = "./upload/" + userInfo.id + '/backup';
+//            var mimetype = ['image/png', 'image/jpeg', 'image/jpeg', 'image/jpg'];
+//            if (mimetype.indexOf(file.mimetype) != -1) {
+            if (!fs.existsSync(dir)) {
+                fs.mkdirSync(dir);
+            } else {
+                async.parallel({
+                    removedir: function (callback) {
+                        rmdir(dir, function (err, dirs, files) {
+                            callback(null, files);
+                        });
+                    }
+                }, function (err, result) {
+                    console.log('directory removed');
+                    fs.mkdirSync(dir);
+                });
+            }
+            extention = path.extname(file.name);
+            filename = "backup-" + new Date().getTime() + extention;
+            file.mv(dir + '/' + filename, function (err) {
+                if (err) {
+                    return next(err);
+                } else {
+//                        imagepath = "/upload/" + userInfo.id + "/" + filename;
+//                        json.image = imagepath;
+                    res.status(config.OK_STATUS).json({message: 'Chat file imported successfully.'});
+                }
+            });
+//            } else {
+//                res.status(config.BAD_REQUEST).json({message: "This File format is not allowed"});
+//            }
+        } else {
+            res.status(config.BAD_REQUEST).json({
+                message: "Please select file first.",
+            });
+        }
+    } else {
+        res.status(config.BAD_REQUEST).json({
+            message: "Validation Error",
+            error: errors
+        });
+    }
+});
 
 
 module.exports = router;
