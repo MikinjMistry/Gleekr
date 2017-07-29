2fe65ebe8cb01715a94f87ffc4471c6bfe868178
sarfalesiddharth <sarfalesiddharth@gmail.com>
2017-07-29 05:14:48 -0700

    Done with export chat file

--- controllers/chat.js
+++ controllers/chat.js
@@ -30,49 +30,57 @@ var _ = require('underscore');
  * @apiError (Error 4xx) {String} message Validation or error message.
  */
 router.post('/import', function (req, res, next) {
-    var errors = req.validationErrors();
-    if (!errors) {
-        var json = req.body;
-        var userInfo = req.userInfo;
-        json.user_id = userInfo.id;
-        if (req.files) {
-            var file = req.files.file;
-            var dir = "./upload/" + userInfo.id + '/backup';
-            var parentdir = "./upload/" + userInfo.id;
+    if (req.files) {
+        var file = req.files.file;
+        var dir = "./upload/" + req.userInfo.id + '/backup';
+        var parentdir = "./upload/" + req.userInfo.id;
 //            var mimetype = ['image/png', 'image/jpeg', 'image/jpeg', 'image/jpg'];
 //            if (mimetype.indexOf(file.mimetype) != -1) {
-            if (!fs.existsSync(parentdir)) {
-                fs.mkdirSync(parentdir);
-            } 
-            if (!fs.existsSync(dir)) {
-                fs.mkdirSync(dir);
+        if (!fs.existsSync(parentdir)) {
+            fs.mkdirSync(parentdir);
+        }
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+        } else {
+            deleteFolderRecursive(dir);
+        }
+
+        extention = path.extname(file.name);
+        filename = "backup-" + new Date().getTime() + extention;
+        file.mv(dir + '/' + filename, function (err) {
+            if (err) {
+                return next(err);
             } else {
-                deleteFolderRecursive(dir);
-            }
-            
-            extention = path.extname(file.name);
-            filename = "backup-" + new Date().getTime() + extention;
-            file.mv(dir + '/' + filename, function (err) {
-                if (err) {
-                    return next(err);
-                } else {
 //                        imagepath = "/upload/" + userInfo.id + "/" + filename;
 //                        json.image = imagepath;
-                    res.status(config.OK_STATUS).json({message: 'Chat file imported successfully.'});
-                }
-            });
+                res.status(config.OK_STATUS).json({message: 'Chat file imported successfully.'});
+            }
+        });
 //            } else {
 //                res.status(config.BAD_REQUEST).json({message: "This File format is not allowed"});
 //            }
-        } else {
-            res.status(config.BAD_REQUEST).json({
-                message: "Please select file first.",
-            });
-        }
     } else {
         res.status(config.BAD_REQUEST).json({
-            message: "Validation Error",
-            error: errors
+            message: "Please select file first.",
+        });
+    }
+});
+/**
+ * @api {get} /export Export your chat file
+ * @apiName Export Chat
+ * @apiGroup Import / Export
+ *
+ * @apiHeader {String}  x-access-token Users unique access-key
+ * 
+ * @apiSuccess (Success 200) {String} message Success message.
+ * @apiError (Error 4xx) {String} message Validation or error message.
+ */
+router.get('/export', function (req, res, next) {
+    var dir = "./upload/" + req.userInfo.id + '/backup';
+    if (fs.existsSync(path)) {
+        fs.readdirSync(path).forEach(function (file, index) {
+            var filepath = path + "/" + file;
+            res.download(filepath);
         });
     }
 });
