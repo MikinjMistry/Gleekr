cd8839cb9de2218a21d6a29da9f723beea9421cd
sarfalesiddharth <sarfalesiddharth@gmail.com>
2017-08-19 23:53:35 -0700

    client changes

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     "node_port": 3000,
-//    "database": "mongodb://admin:wqFB3Svj668k@127.0.0.1:27017/gleekr",
-    "database": "mongodb://127.0.0.1:27017/gleekr",
+    "database": "mongodb://admin:wqFB3Svj668k@127.0.0.1:27017/gleekr",
+    "live_database": "mongodb://admin:wqFB3Svj668k@52.76.246.237:27017/gleekr?authSource=admin",
     "TWILIO_ACCOUNT_SID" : "AC85e236476ea6edc6af5dd63bd677fadd",
     "TWILIO_AUTH_TOKEN" : "aa83bae89551d91d913347f33c40b68f",
     "TWILIO_NUMBER" : "+16629857397",
@@ -12,6 +12,8 @@ module.exports = {
     "MQTT_PASSWORD": "NhXnDz7Vsb3Q",
     "HOST": "127.0.0.1",
     "REMOTE_HOST": "35.154.229.129",
+	"IOS_TEAMID": "C7R3D64FBH",
+    "IOS_KEYID": "GTM5XKR9AG",
     "OK_STATUS": 200,
     "BAD_REQUEST": 400,
     "UNAUTHORIZED": 401,
@@ -21,4 +23,5 @@ module.exports = {
     "DATABASE_ERROR_STATUS": 422,
     "INTERNAL_SERVER_ERROR": 500
 };
+
 
