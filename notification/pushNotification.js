d237b2059ac883c5ebab0d175b750ac3daae8728
sarfalesiddharth <sarfalesiddharth@gmail.com>
2017-08-19 23:44:38 -0700

    client change

--- notification/pushNotification.js
+++ notification/pushNotification.js
@@ -1,25 +1,36 @@
-var notifications = {}
 var apn = require('apn');
+var config = require('../config');
+
+var notifications = {}
 var options = {
     token: {
-        key: "./notification/cert.p8",
-        keyId: "key-id",
-        teamId: "developer-team-id"
+        key: "./notification/AuthKey_GTM5XKR9AG.p8",
+        keyId: config.IOS_KEYID,
+        teamId: config.IOS_TEAMID
     },
-    production: false
+    production: true
 };
+
 apnProvider = new apn.Provider(options);
 notifications.sendMessage = function (messageJSON, deviceToken) {
+
     var note = new apn.Notification();
-    note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
-//    note.badge = 3; 
-//    note.sound = "ping.aiff";
-    note.alert = "You have a new message";
+    note.expiry = Math.floor(Date.now() / 1000) + 24 * 3600; // Expires after 24 hour from now.
+    note.badge = 2;
+    note.sound = "ping.aiff";
+    note.alert = "You have a notification from Gleekr";
     note.payload = messageJSON;
-    
-    apnProvider.send(note, deviceToken).then( result => {
-        console.log(result);
+
+    // Replace this with your app bundle ID:
+    note.topic = "com.domainname.gleekrappPush";
+
+    apnProvider.send(note, deviceToken).then(function (result) {
+        console.log(result.sent);
+        console.log(result.failed);
     });
+
+    // Close the server
+    apnProvider.shutdown();
 }
 module.exports = notifications;
 
